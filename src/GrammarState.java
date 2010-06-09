c910a90ee5d477454269d95ca99ac524ed771c89
Shane Smith <shane.smith.baz@gmail.com>
2010-06-09 09:52:42 -0400

    Removed unused type in GrammarState

--- src/GrammarState.java
+++ src/GrammarState.java
@@ -1,29 +1,15 @@
 
-import java.util.Stack;
-
-
 public class GrammarState {
 
-	public static final int UNKNOWN = -1, EMPTY = 0, TOKEN = 1, RULE = 2;
+	public static final int TOKEN = 1, RULE = 2;
 	
 	private int type;
 	
 	private String name;
 	
-	public GrammarState() {
-		this.type = EMPTY;
-	}
-	
-	public GrammarState(String name) {
-		this.name = name;
-		this.type = UNKNOWN;
-	}
-	
 	public GrammarState(String name, int type) throws Exception {
 		
-		if (type < -1 || type > 2) {
-			throw new Exception("Invalid state type: " + type);
-		}
+		if (type < 1 || type > 2) throw new Exception("Invalid state type: " + type);
 		
 		this.name = name;
 		this.type = type;
@@ -34,11 +20,9 @@ public class GrammarState {
 	
 	public String getTypeString() {
 		switch (type) {
-			case EMPTY: return "-";
 			case TOKEN: return "T";
 			case RULE: return "R";
-			default:	
-			case UNKNOWN: return "?";
+			default: return "?";
 		}
 	}
 	
